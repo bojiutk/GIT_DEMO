0048b71cd827e7e957c61f8ed1d7b486e5b456e3
potato <bojiutk@gmail.com>
2018-08-01 14:56:24 -0500

    Revert "forth  version"
    This reverts commit 871aaee595f53342eb046c971d3f8b9af7b934cf.

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,4 @@
 #version1
-for datafile in ; do   echo $datafile; bash  goostats $datafile stats-$datafile; done
+for datafile in NENE*[AB].txt; do   echo $datafile; bash  goostats $datafile stats-$datafile; done
 #version2 
-wc -l ?  | head -n -10 | tail -2
-#version
-
-over
-
+wc -l *.pdb | head -n -10 | tail -2
