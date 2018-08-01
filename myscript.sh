871aaee595f53342eb046c971d3f8b9af7b934cf
potato <bojiutk@gmail.com>
2018-08-01 14:53:25 -0500

    last version

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,8 @@
 #version1
-for datafile in NENE*[AB].txt; do   echo $datafile; bash  goostats $datafile stats-$datafile; done
+for datafile in ; do   echo $datafile; bash  goostats $datafile stats-$datafile; done
 #version2 
-wc -l *.pdb | head -n -10 | tail -2
+wc -l ?  | head -n -10 | tail -2
+#version
+
+over
+
